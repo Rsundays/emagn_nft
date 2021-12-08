7feba02a65b7facb58ef7f5fe5b55f59112ec5c7
RSundays <arkabal@gmail.com>
2021-12-08 20:27:15 +0100

    enhancements to the gallery added

--- forms.py
+++ forms.py
@@ -1,4 +1,5 @@
 from flask_wtf import FlaskForm
+from flask_ckeditor import CKEditorField
 from wtforms import StringField, SubmitField, SelectField
 from wtforms.validators import DataRequired
 
@@ -7,13 +8,13 @@ from wtforms.validators import DataRequired
 class AddNewForm(FlaskForm):
     title = StringField("Titulo", validators=[DataRequired()])
     size = SelectField("Tipo de cuadro", choices=["Square", "Trip"], validators=[DataRequired()])
-    description = StringField("Descripcion")
+    description = CKEditorField("Descripcion")
     url = StringField("Google Drive URL", validators=[DataRequired()])
     submit = SubmitField("Anadir Cuadro")
 
 
 class UpdateForm(FlaskForm):
     title = StringField("Titulo", validators=[DataRequired()])
-    description = StringField("Descripcion")
+    description = CKEditorField("Descripcion")
     size = SelectField("Tipo de cuadro", choices=["Square", "Trip"], validators=[DataRequired()])
     submit = SubmitField("Actualizar Cuadro")
